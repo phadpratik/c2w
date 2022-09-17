d022ab93195a9baf051fe627b40d81efc55838d9
Pratik Phad <113685321+phadpratik@users.noreply.github.com>
2022-09-17 12:29:11 +0530

    Delete basic directory

--- basic/3.forloop_2sep/dem.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-	int arr[]={1,2,'a','B',55.5555555555555555};
-	for(int i=0;i<5;i++){
-//		printf("%d\n",arr[i]);
-	printf("|%c|",arr[i]);
-	}
-//	printf("|%c|",arr[i]);
-}
